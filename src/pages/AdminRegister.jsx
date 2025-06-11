93934e93bdb576416aedb06087169090f18bc93a
coderstyle56 <kumar.rohitrk2505@gmail.com>
2025-06-11 15:36:49 +0530

    checking

--- src/pages/AdminRegister.jsx
+++ src/pages/AdminRegister.jsx
@@ -35,7 +35,7 @@ const AdminRegister = () => {
     if (!wallet) return alert("Please connect your MetaMask wallet");
 
     try {
-      const res = await axios.post("http://localhost:5000/api/admin/register", {
+      const res = await axios.post("http://localhost:5000/api/admins/register", {
         ...formData,
         wallet,
       });
